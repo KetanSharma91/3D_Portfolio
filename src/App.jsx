1e067a651c8bc0a8056c74d336ae6160e2e183af
KetanSharma91 <ketansharma.ks.911@gmail.com>
2025-06-12 12:32:58 +0530

    commit1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './section/Hero'
+import ShowCaseSection from './section/ShowCaseSection'
+
+function App() {
+  return (
+    <>
+    <Hero />
+    <ShowCaseSection />
+    </>
+  )
+}
+
+export default App
---
+++ src/section/ShowCaseSection.jsx
@@ -0,0 +1,22 @@
+const ShowCaseSection = () => {
+    return (
+        <div id="work" className="app-showcase">
+            <div className="w-full">
+                <div className="showcaselayout">
+                    {/* LEFT  */}
+                    <div className="first-project-wrapper">
+                        <div className="image-wrapper">
+                            <img src="/images/project1.png" alt="Ryde" />
+                        </div>
+                        <div className="text-content">
+                            <h2>ON-</h2>
+                        </div>
+                    </div>
+                    {/* RIGHT */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ShowCaseSection;
