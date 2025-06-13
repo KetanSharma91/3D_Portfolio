8ad2790bc0bdc10014deecadaed31bf18901049d
KetanSharma91 <ketansharma.ks.911@gmail.com>
2025-06-13 16:46:37 +0530

    commit11

--- src/section/ShowCaseSection.jsx
+++ src/section/ShowCaseSection.jsx
@@ -53,7 +53,7 @@ const ShowCaseSection = () => {
                     {/* LEFT  */}
                     <div className="first-project-wrapper" ref={project1Ref}>
                         <div className="image-wrapper">
-                            <img src="/images/project1.png" alt="Ryde" />
+                            <img src="./images/project1.png" alt="Ryde" />
                         </div>
                         <div className="text-content">
                             <h2>On-Demand Rides Made Simple with a Powerful, User-Friendly App called Ryde</h2>
@@ -69,14 +69,14 @@ const ShowCaseSection = () => {
 
                         <div className="project" ref={project2Ref}>
                             <div className="image-wrapper bg-[#ffefdb]">
-                                <img src="/images/project2.png" alt="Library Mangagement Platform" />
+                                <img src="./images/project2.png" alt="Library Mangagement Platform" />
                             </div>
                             <h2>Library Mangement Platform</h2>
                         </div>
 
                         <div className="project" ref={project3Ref}>
                             <div className="image-wrapper bg-[#ffe7eb]">
-                                <img src="/images/project3.png" alt="YC Directory" />
+                                <img src="./images/project3.png" alt="YC Directory" />
                             </div>
                             <h2>YC Directory - A Startup Showcase App</h2>
                         </div>
